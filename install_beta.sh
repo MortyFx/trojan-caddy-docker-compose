0800131575624dc40a7a860931a6eafc265b3784
Patrick SAMA <lawcy@qq.com>
2020-03-27 11:28:34 +0800

    add install script beta version

---
+++ install_beta.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+    systemPackage="yum"
+    systempwd="/usr/lib/systemd/system/"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+    systemPackage="yum"
+    systempwd="/usr/lib/systemd/system/"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+    systemPackage="apt-get"
+    systempwd="/lib/systemd/system/"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+    systemPackage="yum"
+    systempwd="/usr/lib/systemd/system/"
+fi
+
+function install_trojan(){
+
+
+Port80=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 80`
+Port443=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 443`
+if [ -n "$Port80" ]; then
+    process80=`netstat -tlpn | awk -F '[: ]+' '$5=="80"{print $9}'`
+    red "==========================================================="
+    red "检测到80端口被占用，占用进程为：${process80}，本次安装结束"
+    red "==========================================================="
+    exit 1
+fi
+if [ -n "$Port443" ]; then
+    process443=`netstat -tlpn | awk -F '[: ]+' '$5=="443"{print $9}'`
+    red "============================================================="
+    red "检测到443端口被占用，占用进程为：${process443}，本次安装结束"
+    red "============================================================="
+    exit 1
+fi
+CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
+if [ "$CHECK" == "SELINUX=enforcing" ]; then
+    red "======================================================================="
+    red "检测到SELinux为开启状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
+    red "======================================================================="
+    read -p "是否现在重启 ?请输入 [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+	if [[ $yn == [Yy] ]]; then
+	    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+            setenforce 0
+	    echo -e "VPS 重启中..."
+	    reboot
+	fi
+    exit
+fi
+
+if [ "$CHECK" == "SELINUX=permissive" ]; then
+    red "======================================================================="
+    red "检测到SELinux为宽容状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
+    red "======================================================================="
+    read -p "是否现在重启 ?请输入 [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+	if [[ $yn == [Yy] ]]; then
+	    sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config
+            setenforce 0
+	    echo -e "VPS 重启中..."
+	    reboot
+	fi
+    exit
+fi
+
+green "======================="
+blue "请输入trojan服务端的密码"
+green "======================="
+read trojan_passwd
+
+green "======================="
+blue "请输入绑定到本VPS的域名"
+green "======================="
+read your_domain
+real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+local_addr=`curl ipv4.icanhazip.com`
+
+
+if [ $real_addr == $local_addr ] ; then
+	green "=========================================="
+	green "       域名解析正常，开始安装trojan"
+	green "=========================================="
+	sleep 1s
+	
+$systemPackage -y install curl git  >/dev/null 2>&1
+
+
+install_docker
+
+install_docker_compose
+
+git clone https://github.com/Robot-Chen/trojan-caddy-docker-compose.git
+
+cd trojan-caddy-docker-compose
+
+docker-compose down
+
+cat > ./caddy/Caddyfile <<-EOF
+${your_domain}:80 {
+    root /usr/src/trojan
+    log /usr/src/caddy.log
+    index index.html
+}
+${your_domain}:443 {
+    root /usr/src/trojan
+    log /usr/src/caddy.log
+    index index.html
+}
+EOF
+
+
+	rm -rf ./trojan/config/config.json
+	cat > ./trojan/config/config.json <<-EOF
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 443,
+    "remote_addr": "caddy",
+    "remote_port": 80,
+    "password": [
+        "$trojan_passwd"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "cert": "/ssl/$your_domain/$your_domain.crt",
+        "key": "/ssl/$your_domain/$your_domain.key",
+        "key_password": "",
+        "cipher": "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256",
+        "prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "prefer_ipv4": false,
+        "no_delay": true,
+        "keep_alive": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": ""
+    }
+}
+EOF
+	
+
+	docker-compose up -d
+	if [ $? = 0 ]; then
+	green "======================================================================"
+	green "Trojan已安装完成，"
+	blue "域名:${your_domain}"
+	blue "密码: ${trojan_passwd}  "
+	green "2、将下载的压缩包解压，打开文件夹，打开start.bat即打开并运行Trojan客户端"
+	green "3、打开stop.bat即关闭Trojan客户端"
+	green "4、Trojan客户端需要搭配浏览器插件使用，例如switchyomega等"
+	green "======================================================================"
+	else
+    red "==================================="
+	green "1. 重启VPS"
+	green "2. 重新执行脚本"
+	red "==================================="
+	fi
+	
+else
+	red "================================"
+	red "域名解析地址与本VPS IP地址不一致"
+	red "本次安装失败，请确保域名解析正常"
+	red "================================"
+fi
+}
+
+
+function install_docker(){
+
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+systemctl start docker
+systemctl enable docker
+usermod -aG docker $USER
+
+}
+
+function install_docker_compose(){
+
+	$systemPackage -y install  python-pip
+	pip install --upgrade pip
+	pip install docker-compose
+
+	if [ $? = 1 ]; then
+	
+    curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+	ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+fi
+}
+
+function remove_trojan(){
+    red "================================"
+    red "即将卸载trojan"
+    red "================================"
+
+	docker-compose down
+    green "=============="
+    green "trojan删除完毕"
+    green "=============="
+}
+
+start_menu(){
+    clear
+    green " ======================================="
+    green " 介绍：一键安装trojan      "
+    green " 系统：centos7+/debian9+/ubuntu16.04+"
+    blue " 声明："
+    red " *请不要在任何生产环境使用此脚本"
+    red " *请不要有其他程序占用80和443端口"
+    green " ======================================="
+    echo
+    green " 1. 安装trojan"
+    red " 2. 卸载trojan"
+    blue " 0. 退出脚本"
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+    1)
+    install_trojan
+    ;;
+    2)
+    remove_trojan 
+    ;;
+    0)
+    exit 1
+    ;;
+    *)
+    clear
+    red "请输入正确数字"
+    sleep 1s
+    start_menu
+    ;;
+    esac
+}
+
+start_menu
